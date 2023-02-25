9b1c95483c201732cf0aea10d5fc0e79405a51df
abdulGitHb <abdulsamad6876@gmail.com>
2023-02-25 10:07:48 +0530

    initial commit

---
+++ app/src/main/java/com/feedbox/clubcommunity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.feedbox.clubcommunity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
