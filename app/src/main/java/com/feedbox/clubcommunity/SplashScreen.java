b6c679b562045007c73ec6c63e6ef9cf7b88633b
abdulGitHb <abdulsamad6876@gmail.com>
2023-02-26 18:47:20 +0530

    bottom nav added

--- app/src/main/java/com/feedbox/clubcommunity/SplashScreen.java
+++ app/src/main/java/com/feedbox/clubcommunity/SplashScreen.java
@@ -15,14 +15,14 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        View decorView = getWindow().getDecorView();
-        // Hide the status bar.
-        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
-        // Remember that you should never show the action bar if the
-        // status bar is hidden, so hide that too if necessary.
-        ActionBar actionBar = getActionBar();
-        actionBar.hide();
+//        View decorView = getWindow().getDecorView();
+//        // Hide the status bar.
+//        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+//        decorView.setSystemUiVisibility(uiOptions);
+//        // Remember that you should never show the action bar if the
+//        // status bar is hidden, so hide that too if necessary.
+//        ActionBar actionBar = getActionBar();
+//        actionBar.hide();
 
         // This function will keep splash screen for 2 second
         new Handler().postDelayed(() -> {
